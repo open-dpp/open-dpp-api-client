7959dc6915144029e51e6c03e449b32ade057aaf
Florian Bieck <info@florianbieck.com>
2025-02-17 23:20:10 +0100

    Add JavaScript build output and streamline exports
    Updated Rollup config to emit a JavaScript bundle alongside types. Simplified exports in `index.ts` for cleaner re-exports and specified the JS entry point in `package.json`.

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,29 @@
 import typescript from "@rollup/plugin-typescript";
 
-export default {
-  input: "./src/index.ts", // the entry declaration file from tsc
-  output: [
-    {
-      file: "./dist/index.d.ts",
+export default [
+  {
+    input: "./src/index.ts", // the entry declaration file from tsc
+    output: [
+      {
+        file: "./dist/index.d.ts",
+        format: "es",
+      },
+    ],
+    plugins: [typescript()],
+    external: ["axios"],
+  },
+  {
+    input: "./src/index.ts",
+    output: {
+      file: "./dist/index.js",
       format: "es",
+      sourcemap: true,
     },
-  ],
-  plugins: [typescript()],
-  external: ["axios"],
-};
+    plugins: [
+      typescript({
+        declaration: false, // don't emit declarations here
+      }),
+    ],
+    external: ["axios"],
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,3 @@
+// This file exists solely to satisfy module resolution.
+// It is not intended for execution.
+module.exports = {};
